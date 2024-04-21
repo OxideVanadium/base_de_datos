8d8d56b4d012b82258e0e18d9cd943900f3b26cd
Oxide <vanadiumoxide39@gmail.com>
2024-04-21 18:56:17 +0100

    p

--- Unidad_8/Tareas/Tarea_2/pp.sql
+++ Unidad_8/Tareas/Tarea_2/pp.sql
@@ -32,22 +32,26 @@ Función calcular_suma_pedidos_cliente
 Nota:Dado un código de cliente la función debe calcular la suma total de todos los pedidos realizados por el cliente. Deberá hacer uso de la función calcular_precio_total_pedido que ha desarrollado en el apartado anterior.
 Parámetros de entrada: codigo_cliente (INT)
 Parámetros de salida: La suma total de todos los pedidos del cliente (FLOAT) **/
-delimiter $$ 
 
+delimiter $$
 create function calcular_suma_pedidos_cliente(cod_cli int) returns float DETERMINISTIC
 begin
 declare suma_cliente float;
-select sum(dp.codigo_producto*dp.precio_unidad) from detalle_pedido as dp
-join pedido as p on dp.codigo_pedido=p.cod_pedido
-where p.codigo_cliente=cod_cli
-into suma_cliente;
+select sum(precio_total_pedido(codigo_pedido)) from pedido
+where codigo_cliente=cod_cli into suma_cliente;
 return suma_cliente;
 end$$
 
 delimiter ;
 
+
 /**
 select calcular_suma_pedidos_cliente(1);
++----------------------------------+
+| calcular_suma_pedidos_cliente(1) |
++----------------------------------+
+|                            10365 |
++----------------------------------+
 
 
 Función calcular_suma_pagos_cliente
@@ -55,12 +59,59 @@ Nota:Dado un código de cliente la función debe calcular la suma total de los p
 Parámetros de entrada: codigo_cliente (INT)
 Parámetros de salida: La suma total de todos los pagos del cliente (FLOAT) **/
 
+delimiter $$ 
+
+create function calcular_suma_pagos_cliente(cod_cli int) returns float DETERMINISTIC
+begin
+declare suma_cliente float;
+select sum(total) from pago
+where codigo_cliente=cod_cli
+into suma_cliente;
+return suma_cliente;
+end$$
+
+delimiter ;
+
 /**
+
+mysql> select calcular_suma_pagos_cliente(1);
++--------------------------------+
+| calcular_suma_pagos_cliente(1) |
++--------------------------------+
+|                           4000 |
++--------------------------------+
+1 row in set (0.00 sec)
+
 Procedimiento calcular_pagos_pendientes
 Nota:Deberá calcular los pagos pendientes de todos los clientes. Para saber si un cliente tiene algún pago pendiente deberemos calcular cuál es la cantidad de todos
  los pedidos y los pagos que ha realizado. Si la cantidad de los pedidos es mayor que la de los pagos entonces ese cliente tiene pagos pendientes. **/
 
+delimiter $$
+
+create procedure calcular_pagos_pendientes()
+begin
+select p.codigo_cliente, (calcular_suma_pedidos_cliente(pe.codigo_cliente)-calcular_suma_pagos_cliente(p.codigo_cliente)) as debt from pago as p
+join pedido as pe on p.codigo_cliente=pe.codigo_cliente
+join detalle_pedido as dp on pe.codigo_pedido=dp.codigo_pedido
+group by p.codigo_cliente
+having debt>0;
+end$$
+
+delimiter ;
+
+
 /**
+mysql> call calcular_pagos_pendientes();
++----------------+------+
+| codigo_cliente | debt |
++----------------+------+
+|              1 | 6365 |
+|              3 | 2800 |
+|             16 | 3500 |
+|             26 |  133 |
+|             30 | 3500 |
++----------------+------+
+
 Carga de datos
 Realiza la carga de la BBDD de Jardineria y describe los pasos que has realizado.
 **/
